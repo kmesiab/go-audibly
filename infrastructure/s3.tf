b90099e3baa0ba6789e7af3b13d101fb8cc49577
Kevin Mesiab <kmesiab@gmail.com>
2023-10-29 18:56:19 -0700

    GAUD-1001 - Add encryption at rest for s3 bucket

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -4,6 +4,23 @@ resource "aws_s3_bucket" "myp_pre_transcode" {
     Name        = "myp"
     Environment = "dev"
   }
+
+}
+
+resource "aws_kms_key" "s3key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.myp_pre_transcode.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
