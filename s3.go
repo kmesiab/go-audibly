ec439971ce92fdc4ef17a9f6728e8132ce89169f
Kevin Mesiab <kmesiab@gmail.com>
2023-10-31 23:48:04 -0700

    GAUD-0000 - Refactor s3.go

--- s3.go
+++ s3.go
@@ -1,3 +1,4 @@
+// Package main handles the upload and processing of audio files to an AWS S3 bucket.
 package main
 
 import (
@@ -9,40 +10,28 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
+// uploadToS3 uploads a given file to an AWS S3 bucket.
 func uploadToS3(keyName string, appConfig *Config) error {
-	var err error
+	var file *os.File
 	var sess *session.Session
 
 	filePathAndName := fmt.Sprintf("%s/%s", *appConfig.WatchFolder, keyName)
 
-	if sess, err = session.NewSession(&aws.Config{
+	// Initialize AWS session
+	sess, err := session.NewSession(&aws.Config{
 		Credentials: appConfig.AwsCredentials,
 		Region:      appConfig.AwsRegion,
-	}); err != nil {
-		PrepareLogMessagef("Failed to create session: %s", err.Error()).Error()
-
-		return err
-	}
-
-	// #nosec // File set to absolute above
-	file, err := os.Open(filePathAndName)
+	})
 	if err != nil {
 		return err
 	}
 
-	defer func(file *os.File) {
-		e := file.Close()
-		if e != nil {
-			PrepareLogMessagef("Fatal Error: failed to close file").
-				AddError(e).
-				Add("filepath", *appConfig.WatchFolder).
-				Add("filename", file.Name()).
-				Error()
-		}
-	}(file)
+	if file, err = openAudioFile(filePathAndName, appConfig); err != nil {
+		return err
+	}
 
+	// Log and upload
 	PrepareLogMessagef("Uploading file to S3: %s", *appConfig.AwsS3InputBucketName).Debug()
-
 	uploader := s3manager.NewUploader(sess)
 	_, err = uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(*appConfig.AwsS3InputBucketName),
@@ -52,15 +41,37 @@ func uploadToS3(keyName string, appConfig *Config) error {
 
 	if err != nil {
 		return err
-	} else {
-		PrepareLogMessagef("File uploaded: %s", file.Name()).Info()
-
-		completedFileName := fmt.Sprintf("%s/%s", *appConfig.ProcessedAudioFolder, keyName)
-		err := os.Rename(filePathAndName, completedFileName)
-		if err != nil {
-			return err
-		}
 	}
 
-	return nil
+	PrepareLogMessagef("File uploaded: %s", file.Name()).Debug()
+
+	// Move to processed folder
+	return moveToProcessedFolder(keyName, appConfig, filePathAndName)
+}
+
+// moveToProcessedFolder moves the processed file to a specific folder.
+func moveToProcessedFolder(keyName string, appConfig *Config, filePathAndName string) error {
+	completedFileName := fmt.Sprintf("%s/%s", *appConfig.ProcessedAudioFolder, keyName)
+
+	return os.Rename(filePathAndName, completedFileName)
+}
+
+// openAudioFile opens the audio file for reading.
+func openAudioFile(filePathAndName string, appConfig *Config) (*os.File, error) {
+	file, err := os.Open(filePathAndName)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func(file *os.File) {
+		if e := file.Close(); e != nil {
+			PrepareLogMessagef("Fatal Error: failed to close file").
+				AddError(e).
+				Add("filepath", *appConfig.WatchFolder).
+				Add("filename", file.Name()).
+				Error()
+		}
+	}(file)
+
+	return file, nil
 }
